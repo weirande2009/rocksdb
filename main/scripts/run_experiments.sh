3159451d8a1399f570a2a7a1077354a7918dea26
Ran Wei <weiran@rfc1918.address.not.used.bu.edu>
2023-11-29 12:40:55 -0500

    modify experiment

--- main/scripts/run_experiments.sh
+++ main/scripts/run_experiments.sh
@@ -1,5 +1,5 @@
-./scripts/run_for_a_type.sh 6400000 800000 800000 tmp 800 1 mnt/rocksdb/ experiment
-./scripts/run_for_a_type.sh 5600000 1600000 800000 tmp 800 1 mnt/rocksdb/ experiment
-./scripts/run_for_a_type.sh 4800000 2400000 800000 tmp 800 1 mnt/rocksdb/ experiment
-./scripts/run_for_a_type.sh 4000000 3200000 800000 tmp 800 1 mnt/rocksdb/ experiment
-./scripts/run_for_a_type.sh 3200000 4000000 800000 tmp 800 1 mnt/rocksdb/ experiment
+./scripts/run_for_a_type.sh 5600000 800000 1600000 tmp 800 1 mnt/rocksdb/ experiment
+./scripts/run_for_a_type.sh 4800000 1600000 1600000 tmp 800 1 mnt/rocksdb/ experiment
+./scripts/run_for_a_type.sh 4000000 2400000 1600000 tmp 800 1 mnt/rocksdb/ experiment
+./scripts/run_for_a_type.sh 3200000 3200000 1600000 tmp 800 1 mnt/rocksdb/ experiment
+./scripts/run_for_a_type.sh 2400000 4000000 1600000 tmp 800 1 mnt/rocksdb/ experiment
