5601aa0ae22db375faf9d8b07cb222b63e31e7a0
Ran Wei <weiran@Rans-MacBook-Air.local>
2024-01-30 13:50:50 -0500

    add ubuntu experiment

---
+++ main/scripts/run_experiments_ubuntu.sh
@@ -0,0 +1,36 @@
+# run multiple times
+./scripts/run_for_a_type.sh 8000000 0 0 result_multiple_times 800 1 mnt/rocksdb/ experiment 8
+./scripts/run_for_a_type.sh 8000000 0 0 result_multiple_times 800 1 mnt/rocksdb/ experiment 8
+./scripts/run_for_a_type.sh 8000000 0 0 result_multiple_times 800 1 mnt/rocksdb/ experiment 8
+./scripts/run_for_a_type.sh 8000000 0 0 result_multiple_times 800 1 mnt/rocksdb/ experiment 8
+./scripts/run_for_a_type.sh 8000000 0 0 result_multiple_times 800 1 mnt/rocksdb/ experiment 8
+
+./scripts/run_for_a_type.sh 7200000 800000 0 result_multiple_times 800 1 mnt/rocksdb/ experiment 8
+./scripts/run_for_a_type.sh 7200000 800000 0 result_multiple_times 800 1 mnt/rocksdb/ experiment 8
+./scripts/run_for_a_type.sh 7200000 800000 0 result_multiple_times 800 1 mnt/rocksdb/ experiment 8
+./scripts/run_for_a_type.sh 7200000 800000 0 result_multiple_times 800 1 mnt/rocksdb/ experiment 8
+./scripts/run_for_a_type.sh 7200000 800000 0 result_multiple_times 800 1 mnt/rocksdb/ experiment 8
+
+./scripts/run_for_a_type.sh 6400000 1600000 0 result_multiple_times 800 1 mnt/rocksdb/ experiment 8
+./scripts/run_for_a_type.sh 6400000 1600000 0 result_multiple_times 800 1 mnt/rocksdb/ experiment 8
+./scripts/run_for_a_type.sh 6400000 1600000 0 result_multiple_times 800 1 mnt/rocksdb/ experiment 8
+./scripts/run_for_a_type.sh 6400000 1600000 0 result_multiple_times 800 1 mnt/rocksdb/ experiment 8
+./scripts/run_for_a_type.sh 6400000 1600000 0 result_multiple_times 800 1 mnt/rocksdb/ experiment 8
+
+./scripts/run_for_a_type.sh 5600000 2400000 0 result_multiple_times 800 1 mnt/rocksdb/ experiment 8
+./scripts/run_for_a_type.sh 5600000 2400000 0 result_multiple_times 800 1 mnt/rocksdb/ experiment 8
+./scripts/run_for_a_type.sh 5600000 2400000 0 result_multiple_times 800 1 mnt/rocksdb/ experiment 8
+./scripts/run_for_a_type.sh 5600000 2400000 0 result_multiple_times 800 1 mnt/rocksdb/ experiment 8
+./scripts/run_for_a_type.sh 5600000 2400000 0 result_multiple_times 800 1 mnt/rocksdb/ experiment 8
+
+./scripts/run_for_a_type.sh 4800000 3200000 0 result_multiple_times 800 1 mnt/rocksdb/ experiment 8
+./scripts/run_for_a_type.sh 4800000 3200000 0 result_multiple_times 800 1 mnt/rocksdb/ experiment 8
+./scripts/run_for_a_type.sh 4800000 3200000 0 result_multiple_times 800 1 mnt/rocksdb/ experiment 8
+./scripts/run_for_a_type.sh 4800000 3200000 0 result_multiple_times 800 1 mnt/rocksdb/ experiment 8
+./scripts/run_for_a_type.sh 4800000 3200000 0 result_multiple_times 800 1 mnt/rocksdb/ experiment 8
+
+./scripts/run_for_a_type.sh 4000000 4000000 0 result_multiple_times 800 1 mnt/rocksdb/ experiment 8
+./scripts/run_for_a_type.sh 4000000 4000000 0 result_multiple_times 800 1 mnt/rocksdb/ experiment 8
+./scripts/run_for_a_type.sh 4000000 4000000 0 result_multiple_times 800 1 mnt/rocksdb/ experiment 8
+./scripts/run_for_a_type.sh 4000000 4000000 0 result_multiple_times 800 1 mnt/rocksdb/ experiment 8
+./scripts/run_for_a_type.sh 4000000 4000000 0 result_multiple_times 800 1 mnt/rocksdb/ experiment 8
