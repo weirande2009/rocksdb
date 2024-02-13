51614c9292ceb55103f27d1b8e6da07ffde716a5
Ran Wei <weiran@rfc1918.address.not.used.bu.edu>
2024-02-13 14:23:22 -0500

    modify experiment

--- main/scripts/run_experiments.sh
+++ main/scripts/run_experiments.sh
@@ -2,17 +2,26 @@
 # ./scripts/run_for_a_type.sh 8000000 0 0 tmp 800 1 mnt/rocksdb/ experiment 8
 # ./scripts/run_for_a_type.sh 7200000 800000 0 tmp 800 1 mnt/rocksdb/ experiment 8
 # ./scripts/run_for_a_type.sh 6400000 2800000 0 tmp 800 1 mnt/rocksdb/ experiment 8
-./scripts/run_for_a_type.sh 5600000 4000000 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
+# ./scripts/run_for_a_type.sh 5600000 4000000 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
 # ./scripts/run_for_a_type.sh 4800000 4800000 0 tmp 800 1 mnt/rocksdb/ experiment 8
 # ./scripts/run_for_a_type.sh 4000000 6400000 0 tmp 800 1 mnt/rocksdb/ experiment 8
 
 # 5 compactions
-./scripts/run_for_a_type.sh 7000000 0 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
-./scripts/run_for_a_type.sh 6500000 2000000 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
-./scripts/run_for_a_type.sh 6000000 2500000 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
-./scripts/run_for_a_type.sh 5500000 3000000 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
-./scripts/run_for_a_type.sh 5000000 3500000 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
-./scripts/run_for_a_type.sh 4500000 4000000 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
+# ./scripts/run_for_a_type.sh 7000000 0 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
+# ./scripts/run_for_a_type.sh 6500000 2000000 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
+# ./scripts/run_for_a_type.sh 6000000 2500000 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
+# ./scripts/run_for_a_type.sh 5500000 3000000 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
+# ./scripts/run_for_a_type.sh 5000000 3500000 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
+# ./scripts/run_for_a_type.sh 4500000 4000000 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
+
+# 4 compactions
+
+./scripts/run_for_a_type.sh 6500000 0 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
+./scripts/run_for_a_type.sh 6000000 500000 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
+./scripts/run_for_a_type.sh 5500000 1000000 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
+./scripts/run_for_a_type.sh 5000000 1500000 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
+./scripts/run_for_a_type.sh 4500000 2000000 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
+./scripts/run_for_a_type.sh 4000000 3000000 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
 
 # ./scripts/run_for_a_type.sh 8000000 0 0 tmp 800 1 mnt/rocksdb/ experiment 8
 # ./scripts/run_for_a_type.sh 8000000 800000 0 tmp 800 1 mnt/rocksdb/ experiment 8
