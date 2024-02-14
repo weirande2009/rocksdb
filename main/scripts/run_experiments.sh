6240a68e1020409e9b46cda1c2417b45733616ab
Ran Wei <weiran@rfc1918.address.not.used.bu.edu>
2024-02-14 16:09:18 -0500

    modify experiment ubuntu

--- main/scripts/run_experiments.sh
+++ main/scripts/run_experiments.sh
@@ -16,12 +16,18 @@
 
 # 4 compactions
 
-./scripts/run_for_a_type.sh 6500000 0 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
-./scripts/run_for_a_type.sh 6000000 500000 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
-./scripts/run_for_a_type.sh 5500000 1000000 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
-./scripts/run_for_a_type.sh 5000000 1500000 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
-./scripts/run_for_a_type.sh 4500000 2000000 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
-./scripts/run_for_a_type.sh 4000000 3000000 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
+# ./scripts/run_for_a_type.sh 6500000 0 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
+# ./scripts/run_for_a_type.sh 6000000 500000 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
+# ./scripts/run_for_a_type.sh 5500000 1000000 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
+# ./scripts/run_for_a_type.sh 5000000 1500000 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
+# ./scripts/run_for_a_type.sh 4500000 2000000 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
+# ./scripts/run_for_a_type.sh 4000000 3000000 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
+
+# 3 compactions
+./scripts/run_for_a_type.sh 5500000 0 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
+./scripts/run_for_a_type.sh 4000000 3500000 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
+./scripts/run_for_a_type.sh 3500000 4000000 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
+./scripts/run_for_a_type.sh 3000000 4500000 0 tmp/same_compaction_times_5 800 1 mnt/rocksdb/ experiment 8
 
 # ./scripts/run_for_a_type.sh 8000000 0 0 tmp 800 1 mnt/rocksdb/ experiment 8
 # ./scripts/run_for_a_type.sh 8000000 800000 0 tmp 800 1 mnt/rocksdb/ experiment 8
