e0b236bf14f25c283062e859b0f9ffbb633ef828
weiran <1209382452@qq.com>
2024-02-27 21:16:51 -0500

    modify experiment

--- main/scripts/run_experiments.sh
+++ main/scripts/run_experiments.sh
@@ -104,17 +104,17 @@
 ./scripts/run_for_a_type.sh 4800000 3200000 0 tmp/update_normal_50_1 800 1 mnt/roocksdb/ experiment 8 1 --UD\ 1
 ./scripts/run_for_a_type.sh 4000000 4000000 0 tmp/update_normal_50_1 800 1 mnt/rocksdb/ experiment 8 1 --UD\ 1
 
-./scripts/run_for_a_type.sh 7200000 800000 0 tmp/update_normal_50_1 800 1 mnt/rocksdb/ experiment 8 1 --UD\ 2
-./scripts/run_for_a_type.sh 6400000 1600000 0 tmp/update_normal_50_1 800 1 mnt/rocksdb/ experiment 8 1 --UD\ 2
-./scripts/run_for_a_type.sh 5600000 2400000 0 tmp/update_normal_50_1 800 1 mnt/rocksdb/ experiment 8 1 --UD\ 2
-./scripts/run_for_a_type.sh 4800000 3200000 0 tmp/update_normal_50_1 800 1 mnt/roocksdb/ experiment 8 1 --UD\ 2
-./scripts/run_for_a_type.sh 4000000 4000000 0 tmp/update_normal_50_1 800 1 mnt/rocksdb/ experiment 8 1 --UD\ 2
+./scripts/run_for_a_type.sh 7200000 800000 0 tmp/update_beta_1_1 800 1 mnt/rocksdb/ experiment 8 1 --UD\ 2
+./scripts/run_for_a_type.sh 6400000 1600000 0 tmp/update_beta_1_1 800 1 mnt/rocksdb/ experiment 8 1 --UD\ 2
+./scripts/run_for_a_type.sh 5600000 2400000 0 tmp/update_beta_1_1 800 1 mnt/rocksdb/ experiment 8 1 --UD\ 2
+./scripts/run_for_a_type.sh 4800000 3200000 0 tmp/update_beta_1_1 800 1 mnt/roocksdb/ experiment 8 1 --UD\ 2
+./scripts/run_for_a_type.sh 4000000 4000000 0 tmp/update_beta_1_1 800 1 mnt/rocksdb/ experiment 8 1 --UD\ 2
 
-./scripts/run_for_a_type.sh 7200000 800000 0 tmp/update_normal_50_1 800 1 mnt/rocksdb/ experiment 8 1 --UD\ 3
-./scripts/run_for_a_type.sh 6400000 1600000 0 tmp/update_normal_50_1 800 1 mnt/rocksdb/ experiment 8 1 --UD\ 3
-./scripts/run_for_a_type.sh 5600000 2400000 0 tmp/update_normal_50_1 800 1 mnt/rocksdb/ experiment 8 1 --UD\ 3
-./scripts/run_for_a_type.sh 4800000 3200000 0 tmp/update_normal_50_1 800 1 mnt/roocksdb/ experiment 8 1 --UD\ 3
-./scripts/run_for_a_type.sh 4000000 4000000 0 tmp/update_normal_50_1 800 1 mnt/rocksdb/ experiment 8 1 --UD\ 3
+./scripts/run_for_a_type.sh 7200000 800000 0 tmp/update_zipf_1 800 1 mnt/rocksdb/ experiment 8 1 --UD\ 3
+./scripts/run_for_a_type.sh 6400000 1600000 0 tmp/update_zipf_1 800 1 mnt/rocksdb/ experiment 8 1 --UD\ 3
+./scripts/run_for_a_type.sh 5600000 2400000 0 tmp/update_zipf_1 800 1 mnt/rocksdb/ experiment 8 1 --UD\ 3
+./scripts/run_for_a_type.sh 4800000 3200000 0 tmp/update_zipf_1 800 1 mnt/roocksdb/ experiment 8 1 --UD\ 3
+./scripts/run_for_a_type.sh 4000000 4000000 0 tmp/update_zipf_1 800 1 mnt/rocksdb/ experiment 8 1 --UD\ 3
 
 # ./scripts/run_for_a_type.sh 7200000 800000 0 tmp/update_normal_50_1 800 1 mnt/rocksdb/ experiment 8 1 --UD\ 1
 # ./scripts/run_for_a_type.sh 7200000 800000 0 tmp/update_normal_50_1 800 1 mnt/rocksdb/ experiment 8 1 --UD\ 1
