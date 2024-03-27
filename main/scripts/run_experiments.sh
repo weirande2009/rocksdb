7c308d4aa2f74a92fd2610511de041833ab761f7
weiran <1209382452@qq.com>
2024-03-27 00:07:12 -0400

    add experiment

--- main/scripts/run_experiments.sh
+++ main/scripts/run_experiments.sh
@@ -152,4 +152,180 @@
 # ./scripts/run_for_a_type.sh 750000 750000 0 tmp/level4_opt 800 1 mnt/rocksdb experiment 512 1 -L\ 0.0078125
 
 # ./scripts/run_once.sh 5200000 0 0 mnt/rocksdb experiment kMinOverlappingRatio 32 1 -L\ 0.125
-./scripts/run_for_a_type.sh 5200000 0 0 tmp/compaction_12 800 1 mnt/rocksdb experiment 32 1 -L\ 0.125
+# ./scripts/run_for_a_type.sh 5200000 0 0 tmp/compaction_12 800 1 mnt/rocksdb experiment 32 1 -L\ 0.125
+# ./scripts/run_for_a_type.sh 5200000 0 0 tmp/compaction_12 800 1 mnt/rocksdb experiment 32 1 -L\ 0.125
+# ./scripts/run_for_a_type.sh 5200000 0 0 tmp/compaction_12 800 1 mnt/rocksdb experiment 32 1 -L\ 0.125
+
+output_dir=tmp/select_last_similar_for_different_workload/level4_zoe
+
+mkdir -p $output_dir/1500000_512_100_0_0
+mkdir -p $output_dir/1500000_512_90_10_0
+mkdir -p $output_dir/1500000_512_80_20_0
+mkdir -p $output_dir/1500000_512_70_30_0
+mkdir -p $output_dir/1500000_512_60_40_0
+mkdir -p $output_dir/1500000_512_50_50_0
+mkdir -p $output_dir/1500000_512_40_60_0
+mkdir -p $output_dir/1500000_512_30_70_0
+mkdir -p $output_dir/1500000_512_20_80_0
+mkdir -p $output_dir/1500000_512_10_90_0
+
+for k in {1..10}
+do
+    echo $k
+    experiment_name=experiment$k
+    ./scripts/run_multiple_strategy.sh 1500000 0 0 mnt/rocksdb $experiment_name 512 0 0
+    rm -rf $experiment_name/workload.txt
+    mv $experiment_name $output_dir/1500000_512_100_0_0/
+    ./scripts/run_multiple_strategy.sh 1350000 150000 0 mnt/rocksdb $experiment_name 512 0 0
+    rm -rf $experiment_name/workload.txt
+    mv $experiment_name $output_dir/1500000_512_90_10_0/
+    ./scripts/run_multiple_strategy.sh 1200000 300000 0 mnt/rocksdb $experiment_name 512 0 0
+    rm -rf $experiment_name/workload.txt
+    mv $experiment_name $output_dir/1500000_512_80_20_0/
+    ./scripts/run_multiple_strategy.sh 1050000 450000 0 mnt/rocksdb $experiment_name 512 0 0
+    rm -rf $experiment_name/workload.txt
+    mv $experiment_name $output_dir/1500000_512_70_30_0/
+    ./scripts/run_multiple_strategy.sh 900000 600000 0 mnt/rocksdb $experiment_name 512 0 0
+    rm -rf $experiment_name/workload.txt
+    mv $experiment_name $output_dir/1500000_512_60_40_0/
+    ./scripts/run_multiple_strategy.sh 750000 750000 0 mnt/rocksdb $experiment_name 512 0 0
+    rm -rf $experiment_name/workload.txt
+    mv $experiment_name $output_dir/1500000_512_50_50_0/
+    ./scripts/run_multiple_strategy.sh 600000 900000 0 mnt/rocksdb $experiment_name 512 0 0
+    rm -rf $experiment_name/workload.txt
+    mv $experiment_name $output_dir/1500000_512_40_60_0/
+    ./scripts/run_multiple_strategy.sh 450000 1050000 0 mnt/rocksdb $experiment_name 512 0 0
+    rm -rf $experiment_name/workload.txt
+    mv $experiment_name $output_dir/1500000_512_30_70_0/
+    ./scripts/run_multiple_strategy.sh 300000 1200000 0 mnt/rocksdb $experiment_name 512 0 0
+    rm -rf $experiment_name/workload.txt
+    mv $experiment_name $output_dir/1500000_512_20_80_0/
+    ./scripts/run_multiple_strategy.sh 150000 1350000 0 mnt/rocksdb $experiment_name 512 0 0
+    rm -rf $experiment_name/workload.txt
+    mv $experiment_name $output_dir/1500000_512_10_90_0/
+done
+
+output_dir=tmp/select_last_similar_for_different_workload/level4_8_zoe
+
+mkdir -p $output_dir/1500000_512_100_0_0
+mkdir -p $output_dir/1500000_512_90_10_0
+mkdir -p $output_dir/1500000_512_80_20_0
+mkdir -p $output_dir/1500000_512_70_30_0
+mkdir -p $output_dir/1500000_512_60_40_0
+mkdir -p $output_dir/1500000_512_50_50_0
+mkdir -p $output_dir/1500000_512_40_60_0
+mkdir -p $output_dir/1500000_512_30_70_0
+mkdir -p $output_dir/1500000_512_20_80_0
+mkdir -p $output_dir/1500000_512_10_90_0
+
+for k in {1..10}
+do
+    echo $k
+    experiment_name=experiment$k
+    ./scripts/run_multiple_strategy.sh 1500000 0 0 mnt/rocksdb $experiment_name 512 1 -L\ 0.0078125
+    rm -rf $experiment_name/workload.txt
+    mv $experiment_name $output_dir/1500000_512_100_0_0/
+    ./scripts/run_multiple_strategy.sh 1350000 150000 0 mnt/rocksdb $experiment_name 512 1 -L\ 0.0078125
+    rm -rf $experiment_name/workload.txt
+    mv $experiment_name $output_dir/1500000_512_90_10_0/
+    ./scripts/run_multiple_strategy.sh 1200000 300000 0 mnt/rocksdb $experiment_name 512 1 -L\ 0.0078125
+    rm -rf $experiment_name/workload.txt
+    mv $experiment_name $output_dir/1500000_512_80_20_0/
+    ./scripts/run_multiple_strategy.sh 1050000 450000 0 mnt/rocksdb $experiment_name 512 1 -L\ 0.0078125
+    rm -rf $experiment_name/workload.txt
+    mv $experiment_name $output_dir/1500000_512_70_30_0/
+    ./scripts/run_multiple_strategy.sh 900000 600000 0 mnt/rocksdb $experiment_name 512 1 -L\ 0.0078125
+    rm -rf $experiment_name/workload.txt
+    mv $experiment_name $output_dir/1500000_512_60_40_0/
+    ./scripts/run_multiple_strategy.sh 750000 750000 0 mnt/rocksdb $experiment_name 512 1 -L\ 0.0078125
+    rm -rf $experiment_name/workload.txt
+    mv $experiment_name $output_dir/1500000_512_50_50_0/
+    ./scripts/run_multiple_strategy.sh 600000 900000 0 mnt/rocksdb $experiment_name 512 1 -L\ 0.0078125
+    rm -rf $experiment_name/workload.txt
+    mv $experiment_name $output_dir/1500000_512_40_60_0/
+    ./scripts/run_multiple_strategy.sh 450000 1050000 0 mnt/rocksdb $experiment_name 512 1 -L\ 0.0078125
+    rm -rf $experiment_name/workload.txt
+    mv $experiment_name $output_dir/1500000_512_30_70_0/
+    ./scripts/run_multiple_strategy.sh 300000 1200000 0 mnt/rocksdb $experiment_name 512 1 -L\ 0.0078125
+    rm -rf $experiment_name/workload.txt
+    mv $experiment_name $output_dir/1500000_512_20_80_0/
+    ./scripts/run_multiple_strategy.sh 150000 1350000 0 mnt/rocksdb $experiment_name 512 1 -L\ 0.0078125
+    rm -rf $experiment_name/workload.txt
+    mv $experiment_name $output_dir/1500000_512_10_90_0/
+done
+
+# larger workload down to level 3
+output_dir=tmp/select_last_similar_for_different_workload/level3
+mkdir -p $output_dir/8000000_32_100_0_0
+mkdir -p $output_dir/8000000_32_90_10_0
+mkdir -p $output_dir/8000000_32_80_20_0
+mkdir -p $output_dir/8000000_32_70_30_0
+mkdir -p $output_dir/8000000_32_60_40_0
+mkdir -p $output_dir/8000000_32_50_50_0
+mkdir -p $output_dir/8000000_32_40_60_0
+mkdir -p $output_dir/8000000_32_30_70_0
+mkdir -p $output_dir/8000000_32_20_80_0
+mkdir -p $output_dir/8000000_32_10_90_0
+
+for i in {1..10}
+do
+    echo $i
+    experiment_name=experiment$i
+    ./scripts/run_multiple_strategy.sh 8000000 0 0 mnt/rocksdb $experiment_name 32 0 0
+    mv $experiment_name $output_dir/8000000_32_100_0_0/
+    ./scripts/run_multiple_strategy.sh 7200000 800000 0 mnt/rocksdb $experiment_name 32 0 0
+    mv $experiment_name $output_dir/8000000_32_90_10_0/
+    ./scripts/run_multiple_strategy.sh 6400000 1600000 0 mnt/rocksdb $experiment_name 32 0 0
+    mv $experiment_name $output_dir/8000000_32_80_20_0/
+    ./scripts/run_multiple_strategy.sh 5600000 2400000 0 mnt/rocksdb $experiment_name 32 0 0
+    mv $experiment_name $output_dir/8000000_32_70_30_0/
+    ./scripts/run_multiple_strategy.sh 4800000 3200000 0 mnt/rocksdb $experiment_name 32 0 0
+    mv $experiment_name $output_dir/8000000_32_60_40_0/
+    ./scripts/run_multiple_strategy.sh 4000000 4000000 0 mnt/rocksdb $experiment_name 32 0 0
+    mv $experiment_name $output_dir/8000000_32_50_50_0/
+    ./scripts/run_multiple_strategy.sh 3200000 4800000 0 mnt/rocksdb $experiment_name 32 0 0
+    mv $experiment_name $output_dir/8000000_32_40_60_0/
+    ./scripts/run_multiple_strategy.sh 2400000 5600000 0 mnt/rocksdb $experiment_name 32 0 0
+    mv $experiment_name $output_dir/8000000_32_30_70_0/
+    ./scripts/run_multiple_strategy.sh 1600000 6400000 0 mnt/rocksdb $experiment_name 32 0 0
+    mv $experiment_name $output_dir/8000000_32_20_80_0/
+    ./scripts/run_multiple_strategy.sh 800000 7200000 0 mnt/rocksdb $experiment_name 32 0 0
+    mv $experiment_name $output_dir/8000000_32_10_90_0/
+done
+
+mkdir -p $output_dir/1200000_256_100_0_0
+mkdir -p $output_dir/1200000_256_90_10_0
+mkdir -p $output_dir/1200000_256_80_20_0
+mkdir -p $output_dir/1200000_256_70_30_0
+mkdir -p $output_dir/1200000_256_60_40_0
+mkdir -p $output_dir/1200000_256_50_50_0
+mkdir -p $output_dir/1200000_256_40_60_0
+mkdir -p $output_dir/1200000_256_30_70_0
+mkdir -p $output_dir/1200000_256_20_80_0
+mkdir -p $output_dir/1200000_256_10_90_0
+
+for j in {1..10}
+do
+    echo $j
+    experiment_name=experiment$j
+    ./scripts/run_multiple_strategy.sh 1200000 0 0 mnt/rocksdb $experiment_name 256 0 0
+    mv $experiment_name $output_dir/1200000_256_100_0_0/
+    ./scripts/run_multiple_strategy.sh 1080000 120000 0 mnt/rocksdb $experiment_name 256 0 0
+    mv $experiment_name $output_dir/1200000_256_90_10_0/
+    ./scripts/run_multiple_strategy.sh 960000 240000 0 mnt/rocksdb $experiment_name 256 0 0
+    mv $experiment_name $output_dir/1200000_256_80_20_0/
+    ./scripts/run_multiple_strategy.sh 840000 360000 0 mnt/rocksdb $experiment_name 256 0 0
+    mv $experiment_name $output_dir/1200000_256_70_30_0/
+    ./scripts/run_multiple_strategy.sh 720000 480000 0 mnt/rocksdb $experiment_name 256 0 0
+    mv $experiment_name $output_dir/1200000_256_60_40_0/
+    ./scripts/run_multiple_strategy.sh 600000 600000 0 mnt/rocksdb $experiment_name 256 0 0
+    mv $experiment_name $output_dir/1200000_256_50_50_0/
+    ./scripts/run_multiple_strategy.sh 480000 720000 0 mnt/rocksdb $experiment_name 256 0 0
+    mv $experiment_name $output_dir/1200000_256_40_60_0/
+    ./scripts/run_multiple_strategy.sh 360000 840000 0 mnt/rocksdb $experiment_name 256 0 0
+    mv $experiment_name $output_dir/1200000_256_30_70_0/
+    ./scripts/run_multiple_strategy.sh 240000 960000 0 mnt/rocksdb $experiment_name 256 0 0
+    mv $experiment_name $output_dir/1200000_256_20_80_0/
+    ./scripts/run_multiple_strategy.sh 120000 1080000 0 mnt/rocksdb $experiment_name 256 0 0
+    mv $experiment_name $output_dir/1200000_256_10_90_0/
+done
