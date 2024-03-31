69ec032e91aae620b437f53a067fe643edf0c2c1
weiran <1209382452@qq.com>
2024-03-30 23:18:14 -0400

    modify

--- main/scripts/run_experiments_pilaros.sh
+++ main/scripts/run_experiments_pilaros.sh
@@ -50,17 +50,74 @@
 
 # echo 'Running 5 experiments in parallel'
 
-workload_dir=workloads/stability_checking/2000000_0_0_64_8_nvm0
-mkdir -p $workload_dir
-./load_gen --output_path $workload_dir/1.txt -I 2000000 -U 0 -D 0 -E 64 -K 8
-total_bytes=$((2000000 * 64))
+# workload_dir=workloads/stability_checking/2000000_0_0_64_8_nvm0
+# mkdir -p $workload_dir
+# ./load_gen --output_path $workload_dir/1.txt -I 2000000 -U 0 -D 0 -E 64 -K 8
+# total_bytes=$((2000000 * 64))
 
-workspace_dir=workspace/stability_checking/2000000_0_0_64_8_nvm0
+# workspace_dir=workspace/stability_checking/2000000_0_0_64_8_nvm0
+# mkdir -p $workspace_dir
+
+# for i in {1..5}
+# do
+#     ./scripts/run_for_a_type.sh 360 /scratchNVM0/ranw/rocksdb$i/ $workspace_dir/experiment$i $workload_dir/1.txt $total_bytes &
+# done
+
+# echo 'Running 5 experiments in parallel'
+
+workload_dir=workloads/stability_checking/2500000_0_0_64_8_nvm0
+mkdir -p $workload_dir
+./load_gen --output_path $workload_dir/1.txt -I 2500000 -U 0 -D 0 -E 64 -K 8
+total_bytes=$((2500000 * 64))
+
+workspace_dir=workspace/stability_checking/2500000_0_0_64_8_memory/concurrent_6
 mkdir -p $workspace_dir
 
 for i in {1..5}
 do
-    ./scripts/run_for_a_type.sh 360 /scratchNVM0/ranw/rocksdb$i/ $workspace_dir/experiment$i $workload_dir/1.txt $total_bytes &
+    ./scripts/run_for_a_type.sh 360 /mnt/ramd/ranw/rocksdb$i/ $workspace_dir/experiment$i $workload_dir/1.txt $total_bytes &
 done
+./scripts/run_for_a_type.sh 360 /mnt/ramd/ranw/rocksdb$i/ $workspace_dir/experiment$i $workload_dir/1.txt $total_bytes
 
-echo 'Running 5 experiments in parallel'
+echo 'Finished running 6 experiments in parallel'
+
+workspace_dir=workspace/stability_checking/2500000_0_0_64_8_memory/concurrent_5
+mkdir -p $workspace_dir
+
+for i in {1..4}
+do
+    ./scripts/run_for_a_type.sh 360 /mnt/ramd/ranw/rocksdb$i/ $workspace_dir/experiment$i $workload_dir/1.txt $total_bytes &
+done
+./scripts/run_for_a_type.sh 360 /mnt/ramd/ranw/rocksdb$i/ $workspace_dir/experiment$i $workload_dir/1.txt $total_bytes
+
+echo 'Finished running 5 experiments in parallel'
+
+workspace_dir=workspace/stability_checking/2500000_0_0_64_8_memory/concurrent_4
+mkdir -p $workspace_dir
+
+for i in {1..3}
+do
+    ./scripts/run_for_a_type.sh 360 /mnt/ramd/ranw/rocksdb$i/ $workspace_dir/experiment$i $workload_dir/1.txt $total_bytes &
+done
+./scripts/run_for_a_type.sh 360 /mnt/ramd/ranw/rocksdb$i/ $workspace_dir/experiment$i $workload_dir/1.txt $total_bytes
+
+echo 'Finished running 4 experiments in parallel'
+
+workspace_dir=workspace/stability_checking/2500000_0_0_64_8_memory/concurrent_3
+mkdir -p $workspace_dir
+
+for i in {1..2}
+do
+    ./scripts/run_for_a_type.sh 360 /mnt/ramd/ranw/rocksdb$i/ $workspace_dir/experiment$i $workload_dir/1.txt $total_bytes &
+done
+./scripts/run_for_a_type.sh 360 /mnt/ramd/ranw/rocksdb$i/ $workspace_dir/experiment$i $workload_dir/1.txt $total_bytes
+
+echo 'Finished running 3 experiments in parallel'
+
+workspace_dir=workspace/stability_checking/2500000_0_0_64_8_memory/concurrent_2
+mkdir -p $workspace_dir
+
+./scripts/run_for_a_type.sh 360 /mnt/ramd/ranw/rocksdb1/ $workspace_dir/experiment1 $workload_dir/1.txt $total_bytes &
+./scripts/run_for_a_type.sh 360 /mnt/ramd/ranw/rocksdb2/ $workspace_dir/experiment2 $workload_dir/1.txt $total_bytes
+
+echo 'Finished running 2 experiments in parallel'
