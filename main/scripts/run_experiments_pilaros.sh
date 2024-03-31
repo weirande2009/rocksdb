3e19ae0aeea98e9db1f9197b682c9282f44d113c
weiran <1209382452@qq.com>
2024-03-30 20:08:35 -0400

    modify

--- main/scripts/run_experiments_pilaros.sh
+++ main/scripts/run_experiments_pilaros.sh
@@ -27,6 +27,7 @@ mkdir -p $workload_dir
 total_bytes=128000000
 
 workspace_dir=workspace/test/2000000_0_0_64_0125
+mkdir -p $workspace_dir
 
 ./scripts/run_once_existing.sh /scratchNVM1/ranw/rocksdb1/ $workspace_dir/experiment1 kMinOverlappingRatio $total_bytes $workload_dir/1.txt &
 ./scripts/run_once_existing.sh /scratchNVM1/ranw/rocksdb2/ $workspace_dir/experiment2 kMinOverlappingRatio $total_bytes $workload_dir/1.txt &
