7be0d77cfeeedbf35f36e30a48dc65b688b405cd
weiran <1209382452@qq.com>
2024-03-30 20:42:05 -0400

    modify

--- main/scripts/run_experiments_pilaros.sh
+++ main/scripts/run_experiments_pilaros.sh
@@ -21,16 +21,30 @@
 # hdd: 9.863s
 
 # test concurrent runs' consistency in memory
-workload_dir=workloads/test/2000000_0_0_64_0125
+# workload_dir=workloads/test/2000000_0_0_64_0125
+# mkdir -p $workload_dir
+# ./load_gen --output_path $workload_dir/1.txt -I 2000000 -U 0 -D 0 -E 64 -K 8
+# total_bytes=$((2000000 * 64))
+
+# workspace_dir=workspace/test/2000000_0_0_64_0125
+# mkdir -p $workspace_dir
+
+# ./scripts/run_once_existing.sh /mnt/ramd/ranw/rocksdb1/ $workspace_dir/experiment1 kMinOverlappingRatio $total_bytes $workload_dir/1.txt &
+# ./scripts/run_once_existing.sh /mnt/ramd/ranw/rocksdb2/ $workspace_dir/experiment2 kMinOverlappingRatio $total_bytes $workload_dir/1.txt &
+# ./scripts/run_once_existing.sh /mnt/ramd/ranw/rocksdb3/ $workspace_dir/experiment3 kMinOverlappingRatio $total_bytes $workload_dir/1.txt &
+# ./scripts/run_once_existing.sh /mnt/ramd/ranw/rocksdb4/ $workspace_dir/experiment4 kMinOverlappingRatio $total_bytes $workload_dir/1.txt
+
+# test concurrent enumeration runs' consistency in memory
+workload_dir=workloads/test/2000000_0_0_64_8
 mkdir -p $workload_dir
 ./load_gen --output_path $workload_dir/1.txt -I 2000000 -U 0 -D 0 -E 64 -K 8
-total_bytes=128000000
+total_bytes=$((2000000 * 64))
 
-workspace_dir=workspace/test/2000000_0_0_64_0125
+workspace_dir=workspace/test/2000000_0_0_64_8
 mkdir -p $workspace_dir
 
-./scripts/run_once_existing.sh /mnt/ramd/ranw/rocksdb1/ $workspace_dir/experiment1 kMinOverlappingRatio $total_bytes $workload_dir/1.txt &
-./scripts/run_once_existing.sh /mnt/ramd/ranw/rocksdb2/ $workspace_dir/experiment2 kMinOverlappingRatio $total_bytes $workload_dir/1.txt &
-./scripts/run_once_existing.sh /mnt/ramd/ranw/rocksdb3/ $workspace_dir/experiment3 kMinOverlappingRatio $total_bytes $workload_dir/1.txt &
-./scripts/run_once_existing.sh /mnt/ramd/ranw/rocksdb4/ $workspace_dir/experiment4 kMinOverlappingRatio $total_bytes $workload_dir/1.txt
+for i in {1..5}
+    ./scripts/run_for_a_type.sh 360 /mnt/ramd/ranw/rocksdb$i/ $workspace_dir/experiment$i $workload_dir/$i.txt $total_bytes &
+done
 
+echo 'Running 5 experiments in parallel'
--- main/scripts/run_for_a_type.sh
+++ main/scripts/run_for_a_type.sh
@@ -3,33 +3,25 @@
 source ./scripts/tools.sh
 source ./scripts/run_workload.sh
 
-if ! [ $# -eq 6 ]; then
+if ! [ $# -eq 5 ]; then
     echo 'in this shell script, there will be eleven parameters, which are:'
-    echo '1. the path to save the experiments result'
-    echo '2. the number of times to run kEnumerate for each workload'
-    echo '3. the path of rocksdb'
-    echo '4. the path of the experiment workspace'
-    echo '5. the workload path'
-    echo '6. the number of all inserted bytes'
+    echo '1. the number of times to run kEnumerate for each workload'
+    echo '2. the path of rocksdb'
+    echo '3. the path of the experiment workspace'
+    echo '4. the workload path'
+    echo '5. the number of all inserted bytes'
     exit 1
 fi
 
 # check whether workload.txt exists
-if [ ! -e $5 ]; then
+if [ ! -e $4 ]; then
     echo "workload does not exist"
     exit -1
 fi
 
-if [ ! -d $1 ]; then
-    mkdir -p $1
-fi
-
 # initialize the workspace
-initialize_workspace $4
+initialize_workspace $3
 
-run_all_baselines $6 $3 $4 $5
+run_all_baselines $5 $2 $3 $4
 
-run_enumerate $2 $6 $3 $4 $5
-
-# copy the experiment result to a given folder
-mv $4 $1
+run_enumerate $1 $5 $2 $3 $4
