7c0ce2b4f667771d3eff47c8f402cb611bd8a80a
weiran <1209382452@qq.com>
2024-03-30 22:37:03 -0400

    modify experiment

--- main/scripts/run_experiments_pilaros.sh
+++ main/scripts/run_experiments_pilaros.sh
@@ -35,17 +35,32 @@
 # ./scripts/run_once_existing.sh /mnt/ramd/ranw/rocksdb4/ $workspace_dir/experiment4 kMinOverlappingRatio $total_bytes $workload_dir/1.txt
 
 # test concurrent enumeration runs' consistency in memory
-workload_dir=workloads/test/2000000_0_0_64_8
+# workload_dir=workloads/test/2000000_0_0_64_8
+# mkdir -p $workload_dir
+# ./load_gen --output_path $workload_dir/1.txt -I 2000000 -U 0 -D 0 -E 64 -K 8
+# total_bytes=$((2000000 * 64))
+
+# workspace_dir=workspace/test/2000000_0_0_64_8
+# mkdir -p $workspace_dir
+
+# for i in {1..5}
+# do
+#     ./scripts/run_for_a_type.sh 360 /mnt/ramd/ranw/rocksdb$i/ $workspace_dir/experiment$i $workload_dir/1.txt $total_bytes &
+# done
+
+# echo 'Running 5 experiments in parallel'
+
+workload_dir=workloads/stability_checking/2000000_0_0_64_8_nvm0
 mkdir -p $workload_dir
 ./load_gen --output_path $workload_dir/1.txt -I 2000000 -U 0 -D 0 -E 64 -K 8
 total_bytes=$((2000000 * 64))
 
-workspace_dir=workspace/test/2000000_0_0_64_8
+workspace_dir=workspace/stability_checking/2000000_0_0_64_8_nvm0
 mkdir -p $workspace_dir
 
 for i in {1..5}
 do
-    ./scripts/run_for_a_type.sh 360 /mnt/ramd/ranw/rocksdb$i/ $workspace_dir/experiment$i $workload_dir/1.txt $total_bytes &
+    ./scripts/run_for_a_type.sh 360 /scratchNVM0/ranw/rocksdb$i/ $workspace_dir/experiment$i $workload_dir/1.txt $total_bytes &
 done
 
 echo 'Running 5 experiments in parallel'
