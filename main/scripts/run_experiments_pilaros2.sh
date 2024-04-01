08f88028986c8c10f26d0328671faff023fa8263
weiran <1209382452@qq.com>
2024-04-01 14:06:02 -0400

    modify

--- main/scripts/run_experiments_pilaros2.sh
+++ main/scripts/run_experiments_pilaros2.sh
@@ -6,11 +6,11 @@ total_bytes=$((2000000 * 64))
 workspace_dir=workspace/stability_checking/2000000_0_0_64_8_nvm1/concurrent_6
 mkdir -p $workspace_dir
 
-# for i in {1..5}
-# do
-#     ./scripts/run_for_a_type.sh 500 /scratchNVM1/ranw/rocksdb$i/ $workspace_dir/run$i $workload_dir/1.txt $total_bytes 0 &
-# done
-./scripts/run_for_a_type.sh 10 /scratchNVM1/ranw/rocksdb6/ $workspace_dir/run7 $workload_dir/1.txt $total_bytes 0
+for i in {8..12}
+do
+    ./scripts/run_for_a_type.sh 10 /scratchNVM1/ranw/rocksdb$i/ $workspace_dir/run$i $workload_dir/1.txt $total_bytes 0 &
+done
+./scripts/run_for_a_type.sh 10 /scratchNVM1/ranw/rocksdb6/ $workspace_dir/run13 $workload_dir/1.txt $total_bytes 0
 
 echo 'Finished running 6 experiments in parallel'
 
