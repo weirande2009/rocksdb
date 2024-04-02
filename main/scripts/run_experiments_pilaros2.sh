78b8278ba8365ecedc5e015c0f47c44a5146b780
weiran <1209382452@qq.com>
2024-04-02 11:33:18 -0400

    add

--- main/scripts/run_experiments_pilaros2.sh
+++ main/scripts/run_experiments_pilaros2.sh
@@ -1,17 +1,18 @@
-# workload_dir=workloads/stability_checking/2000000_0_0_64_8_nvm1/concurrent_6
-# mkdir -p $workload_dir
-# # ./load_gen --output_path $workload_dir/1.txt -I 2000000 -U 0 -D 0 -E 64 -K 8
-# total_bytes=$((2000000 * 64))
+workload_dir=workloads/stability_checking/2000000_0_0_64_8_memory_both/concurrent_6
+mkdir -p $workload_dir
+./load_gen --output_path $workload_dir/1.txt -I 2000000 -U 0 -D 0 -E 64 -K 8
+total_bytes=$((2000000 * 64))
 
 workspace_dir=workspace/stability_checking/2000000_0_0_64_8_memory_both/concurrent_6
 mkdir -p $workspace_dir
-./load_gen --output_path $workspace_dir/1.txt -I 2000000 -U 0 -D 0 -E 64 -K 8
 
+enumeration_runs=100
+rocksdb_dir=/mnt/ramd/ranw
 for i in {1..5}
 do
-    ./scripts/run_for_a_type.sh 500 /scratchNVM1/ranw/rocksdb$i/ $workspace_dir/run$i $workload_dir/1.txt $total_bytes 0 &
+    ./scripts/run_for_a_type.sh $enumeration_runs $rocksdb_dir/rocksdb$i/ $workspace_dir/run$i $workload_dir/1.txt $total_bytes 0 &
 done
-./scripts/run_for_a_type.sh 500 /scratchNVM1/ranw/rocksdb6/ $workspace_dir/run6 $workload_dir/1.txt $total_bytes 0
+./scripts/run_for_a_type.sh $enumeration_runs $rocksdb_dir/rocksdb6/ $workspace_dir/run6 $workload_dir/1.txt $total_bytes 0
 
 # echo 'Finished running 6 experiments in parallel'
 
