8285b0241ee286bec3ad2f06dd6e87ef178f79fd
weiran <1209382452@qq.com>
2024-04-08 16:17:26 -0400

    add

--- main/scripts/run_experiments_pilaros_experiment1.sh
+++ main/scripts/run_experiments_pilaros_experiment1.sh
@@ -1,5 +1,5 @@
 # Experiment on 4 baseline policies and the optimal policy
-# workloads settings: 8 bytes key size, 64 bytes entry size, 2000000 operations
+# workloads settings: 8 bytes key size, 64 bytes entry size, 2500000 operations
 # workload 1: 100% insert, 0% update
 # workload 2: 90% insert, 10% update
 # workload 3: 80% insert, 20% update
@@ -15,3 +15,61 @@
 # 2. kRoundRobin are stable and good
 # 3. kMinOverlappingRatio are not stable, but generally good
 # 4. kOldestLargestSeqFirst and kOldestSmallestSeqFirst are neither stable nor good
+
+source ./scripts/run_workload.sh
+
+run_multiple_times_for_a_type() {
+    if ! [ $# -eq 3 ]; then
+        echo 'in this shell script, there will be three parameters, which are:'
+        echo '1. percentage_insert'
+        echo '2. percentage_update'
+        echo '3. the number of workloads'
+        exit 1
+    fi
+
+    write_buffer_size=$((8 * 1024 * 1024))
+    target_file_size_base=$((8 * 1024 * 1024))
+    target_file_number=4
+
+    percentage_insert=$1
+    percentage_update=$2
+    n_workloads=$3
+    
+    entry_size=64
+    num_operation=3000000
+    workload_size=$((num_operation * entry_size))
+
+    num_insert=$((num_operation * percentage_insert / 100))
+    num_update=$((num_operation * percentage_update / 100))
+    experiment_root_name=compare_optimal_with_baselines
+    experiment_name=${num_insert}_${num_update}_0_${entry_size}_8_memory/first_run/${percentage_insert}_${percentage_update}
+    dir_name=$experiment_root_name/$experiment_name
+    workload_dir=workloads/$dir_name
+    workspace_dir=workspace/$dir_name
+    mkdir -p $workload_dir
+    mkdir -p $workspace_dir
+    
+    rocksdb_dir=/mnt/ramd/ranw/$dir_name
+    mkdir -p $rocksdb_dir
+
+    enumeration_runs=15000
+
+    for i in $(seq 1 $n_workloads)
+    do  
+        ./load_gen --output_path $workload_dir/${i}.txt -I $num_insert -U $num_update -D 0 -E $entry_size -K 8
+
+        run_for_a_type $enumeration_runs $rocksdb_dir/rocksdb$i/ $workspace_dir/run$i $workload_dir/${i}.txt $workload_size 1 0 $write_buffer_size $target_file_size_base $target_file_number
+    done
+
+    rm -rf $rocksdb_dir
+}
+
+num_workloads=1
+
+run_multiple_times_for_a_type 100 0 $num_workloads & # workload 1: 100% insert, 0% update
+run_multiple_times_for_a_type 90 10 $num_workloads & # workload 2: 90% insert, 10% update
+run_multiple_times_for_a_type 80 20 $num_workloads & # workload 3: 80% insert, 20% update
+run_multiple_times_for_a_type 70 30 $num_workloads & # workload 4: 70% insert, 30% update
+run_multiple_times_for_a_type 60 40 $num_workloads & # workload 5: 60% insert, 40% update
+run_multiple_times_for_a_type 50 50 $num_workloads   # workload 6: 50% insert, 50% update
+
