1d04146687d75335588c92cd01986e2ccb0e4068
weiran <1209382452@qq.com>
2024-05-18 23:21:19 -0400

    add

--- main/scripts/edbt/all.sh
+++ main/scripts/edbt/all.sh
@@ -1,6 +1,6 @@
-./scripts/edbt/distribution.sh > distribution.txt
-./scripts/edbt/distribution_delete.sh > distribution_delete.txt
-./scripts/edbt/device.sh > device.txt
-./scripts/edbt/device_delete.sh > device_delete.txt
+# ./scripts/edbt/distribution.sh > distribution.txt
+# ./scripts/edbt/distribution_delete.sh > distribution_delete.txt
+# ./scripts/edbt/device.sh > device.txt
+# ./scripts/edbt/device_delete.sh > device_delete.txt
 ./scripts/edbt/scalability.sh > scalability.txt
 ./scripts/edbt/delete.sh > delete.txt
