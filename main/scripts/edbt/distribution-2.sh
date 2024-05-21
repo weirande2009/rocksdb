27d95fd352f2805cb68c2651fe3fe521f9b13eef
weiran <1209382452@qq.com>
2024-05-21 15:22:01 -0400

    add

--- main/scripts/edbt/distribution-2.sh
+++ main/scripts/edbt/distribution-2.sh
@@ -60,56 +60,56 @@ run_multiple_times_for_baseline() {
 update_distribution() {
     num_workloads=10
 
-    # normal distribution
-    rocksdb_root_dir=/scratchNVM1/ranw/update/normal1
-    experiment_name=update_distribution_influence/normal1
-    run_multiple_times_for_baseline 50 50 0 $num_workloads $rocksdb_root_dir --UD\ 1\ --UD_NMP\ 0.5\ --UD_NDEV\ 0.001 $experiment_name &
+    # # normal distribution
+    # rocksdb_root_dir=/scratchNVM1/ranw/update/normal1
+    # experiment_name=update_distribution_influence/normal1
+    # run_multiple_times_for_baseline 50 50 0 $num_workloads $rocksdb_root_dir --UD\ 1\ --UD_NMP\ 0.5\ --UD_NDEV\ 0.001 $experiment_name &
 
-    rocksdb_root_dir=/scratchNVM1/ranw/update/normal2
-    experiment_name=update_distribution_influence/normal2
-    run_multiple_times_for_baseline 50 50 0 $num_workloads $rocksdb_root_dir --UD\ 1\ --UD_NMP\ 0.5\ --UD_NDEV\ 0.01 $experiment_name &
+    # rocksdb_root_dir=/scratchNVM1/ranw/update/normal2
+    # experiment_name=update_distribution_influence/normal2
+    # run_multiple_times_for_baseline 50 50 0 $num_workloads $rocksdb_root_dir --UD\ 1\ --UD_NMP\ 0.5\ --UD_NDEV\ 0.01 $experiment_name &
 
-    rocksdb_root_dir=/scratchNVM1/ranw/update/normal3
-    experiment_name=update_distribution_influence/normal3
-    run_multiple_times_for_baseline 50 50 0 $num_workloads $rocksdb_root_dir --UD\ 1\ --UD_NMP\ 0.5\ --UD_NDEV\ 0.1 $experiment_name &
+    # rocksdb_root_dir=/scratchNVM1/ranw/update/normal3
+    # experiment_name=update_distribution_influence/normal3
+    # run_multiple_times_for_baseline 50 50 0 $num_workloads $rocksdb_root_dir --UD\ 1\ --UD_NMP\ 0.5\ --UD_NDEV\ 0.1 $experiment_name &
 
-    rocksdb_root_dir=/scratchNVM1/ranw/update/normal4
-    experiment_name=update_distribution_influence/normal4
-    run_multiple_times_for_baseline 50 50 0 $num_workloads $rocksdb_root_dir --UD\ 1\ --UD_NMP\ 0.5\ --UD_NDEV\ 1 $experiment_name &
+    # rocksdb_root_dir=/scratchNVM1/ranw/update/normal4
+    # experiment_name=update_distribution_influence/normal4
+    # run_multiple_times_for_baseline 50 50 0 $num_workloads $rocksdb_root_dir --UD\ 1\ --UD_NMP\ 0.5\ --UD_NDEV\ 1 $experiment_name &
 
-    rocksdb_root_dir=/scratchNVM1/ranw/update/normal5
-    experiment_name=update_distribution_influence/normal5
-    run_multiple_times_for_baseline 50 50 0 $num_workloads $rocksdb_root_dir --UD\ 1\ --UD_NMP\ 0.5\ --UD_NDEV\ 10 $experiment_name &
+    # rocksdb_root_dir=/scratchNVM1/ranw/update/normal5
+    # experiment_name=update_distribution_influence/normal5
+    # run_multiple_times_for_baseline 50 50 0 $num_workloads $rocksdb_root_dir --UD\ 1\ --UD_NMP\ 0.5\ --UD_NDEV\ 10 $experiment_name &
 
-    rocksdb_root_dir=/scratchNVM1/ranw/update/normal6
-    experiment_name=update_distribution_influence/normal6
-    run_multiple_times_for_baseline 50 50 0 $num_workloads $rocksdb_root_dir --UD\ 1\ --UD_NMP\ 0.5\ --UD_NDEV\ 100 $experiment_name &
+    # rocksdb_root_dir=/scratchNVM1/ranw/update/normal6
+    # experiment_name=update_distribution_influence/normal6
+    # run_multiple_times_for_baseline 50 50 0 $num_workloads $rocksdb_root_dir --UD\ 1\ --UD_NMP\ 0.5\ --UD_NDEV\ 100 $experiment_name &
 
-    wait $(jobs -p)
+    # wait $(jobs -p)
 
     # zipfian distribution
     rocksdb_root_dir=/scratchNVM1/ranw/update/zipfian1
     experiment_name=update_distribution_influence/zipfian1
     run_multiple_times_for_baseline 50 50 0 $num_workloads $rocksdb_root_dir --UD\ 3\ --UD_ZALPHA\ 0.1 $experiment_name &
 
-    rocksdb_root_dir=/scratchNVM1/ranw/update/zipfian1
-    experiment_name=update_distribution_influence/zipfian1
+    rocksdb_root_dir=/scratchNVM1/ranw/update/zipfian2
+    experiment_name=update_distribution_influence/zipfian2
     run_multiple_times_for_baseline 50 50 0 $num_workloads $rocksdb_root_dir --UD\ 3\ --UD_ZALPHA\ 0.4 $experiment_name &
 
-    rocksdb_root_dir=/scratchNVM1/ranw/update/zipfian1
-    experiment_name=update_distribution_influence/zipfian1
+    rocksdb_root_dir=/scratchNVM1/ranw/update/zipfian3
+    experiment_name=update_distribution_influence/zipfian3
     run_multiple_times_for_baseline 50 50 0 $num_workloads $rocksdb_root_dir --UD\ 3\ --UD_ZALPHA\ 0.7 $experiment_name &
 
-    rocksdb_root_dir=/scratchNVM1/ranw/update/zipfian1
-    experiment_name=update_distribution_influence/zipfian1
+    rocksdb_root_dir=/scratchNVM1/ranw/update/zipfian4
+    experiment_name=update_distribution_influence/zipfian4
     run_multiple_times_for_baseline 50 50 0 $num_workloads $rocksdb_root_dir --UD\ 3\ --UD_ZALPHA\ 1.0 $experiment_name &
 
-    rocksdb_root_dir=/scratchNVM1/ranw/update/zipfian1
-    experiment_name=update_distribution_influence/zipfian1
+    rocksdb_root_dir=/scratchNVM1/ranw/update/zipfian5
+    experiment_name=update_distribution_influence/zipfian5
     run_multiple_times_for_baseline 50 50 0 $num_workloads $rocksdb_root_dir --UD\ 3\ --UD_ZALPHA\ 1.3 $experiment_name &
 
-    rocksdb_root_dir=/scratchNVM1/ranw/update/zipfian1
-    experiment_name=update_distribution_influence/zipfian1
+    rocksdb_root_dir=/scratchNVM1/ranw/update/zipfian6
+    experiment_name=update_distribution_influence/zipfian6
     run_multiple_times_for_baseline 50 50 0 $num_workloads $rocksdb_root_dir --UD\ 3\ --UD_ZALPHA\ 1.6 $experiment_name &
 
     wait $(jobs -p)
