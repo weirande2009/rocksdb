ac5165aad3372e54671ec7a41d4cbde6938a03b9
weiran <1209382452@qq.com>
2024-05-22 23:23:03 -0400

    add

---
+++ main/scripts/run_for_a_type.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+source ./scripts/run_workload.sh
+
+if ! [ $# -eq 10 ]; then
+    echo 'in this shell script, there will be six parameters, which are:'
+    echo '1. the number of times to run kEnumerate for each workload'
+    echo '2. the path of rocksdb'
+    echo '3. the path of the experiment workspace'
+    echo '4. the workload path'
+    echo '5. the number of all inserted bytes'
+    echo '6. whether to skip trivial move'
+    echo '7. whether to skip extend non-l0 trivial move'
+    echo '8. write buffer size'
+    echo '9. target file size base'
+    echo '10. target file number' 
+    exit 1
+fi
+
+# check whether workload.txt exists
+if [ ! -e $4 ]; then
+    echo "workload does not exist"
+    exit -1
+fi
+
+# initialize the workspace
+initialize_workspace $3
+
+run_all_baselines $5 $2 $3 $4 $8 $9 ${10}
+
+time run_enumerate $1 $5 $2 $3 $4 $6 $7 $8 $9 ${10} > $3/out.txt
