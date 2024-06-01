c81f88ee57b2c0b3bc2cabb515f033963210a416
weiran <1209382452@qq.com>
2024-06-01 01:22:15 -0400

    add

--- main/scripts/run_workload.sh
+++ main/scripts/run_workload.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 run_once() {
-    if ! [ $# -eq 8 ]; then
+    if ! [ $# -eq 9 ]; then
         echo 'in this function, there will be three parameters, which are:'
         echo '1. the number of all inserted bytes'
         echo '2. the path of the rocksdb'
@@ -11,10 +11,12 @@ run_once() {
         echo '6. write buffer size'
         echo '7. target file size base'
         echo '8. target file number' 
+        echo '9. write buffer data structure'
         exit 1
     fi
     find $2 -mindepth 1 -delete
-    ./simple_example $4 $1 $2 $3 $5 0 0 $6 $7 $8
+    ./simple_example $4 $1 $2 $3 $5 0 0 $6 $7 $8 $9
+    cp $2/LOG $3/LOG
 }
 
 run_baseline() {
@@ -121,6 +123,44 @@ run_all_baselines_2() {
     cp $2/LOG $3/LOG_SLS
     rocksdb_size=$(du -sk $2 | awk '{ printf "%dK\n", $1 }')
     echo "kSelectLastSimilar: $rocksdb_size" >> $3/rocksdb_size.txt
+}
+
+run_all_baselines_3() {
+    if ! [ $# -eq 8 ]; then
+        echo 'in this shell script, there will be three parameters, which are:'
+        echo '1. the number of all inserted bytes'
+        echo '2. the path of the rocksdb'
+        echo '3. the path of the experiment workspace'
+        echo '4. the workload path'
+        echo '5. write buffer size'
+        echo '6. target file size base'
+        echo '7. target file number' 
+        echo '8. write buffer data structure'
+        exit 1
+    fi
+    find $2 -mindepth 1 -delete
+    ./simple_example kRoundRobin $1 $2 $3 $4 0 0 $5 $6 $7 $8
+    cp $2/LOG $3/LOG_RR
+    rocksdb_size=$(du -sk $2 | awk '{ printf "%dK\n", $1 }')
+    echo "kRoundRobin: $rocksdb_size" >> $3/rocksdb_size.txt
+
+    find $2 -mindepth 1 -delete
+    ./simple_example kMinOverlappingRatio $1 $2 $3 $4 0 0 $5 $6 $7 $8
+    cp $2/LOG $3/LOG_MOR
+    rocksdb_size=$(du -sk $2 | awk '{ printf "%dK\n", $1 }')
+    echo "kMinOverlappingRatio: $rocksdb_size" >> $3/rocksdb_size.txt
+
+    find $2 -mindepth 1 -delete
+    ./simple_example kOldestLargestSeqFirst $1 $2 $3 $4 0 0 $5 $6 $7 $8
+    cp $2/LOG $3/LOG_OLSF
+    rocksdb_size=$(du -sk $2 | awk '{ printf "%dK\n", $1 }')
+    echo "kOldestLargestSeqFirst: $rocksdb_size" >> $3/rocksdb_size.txt
+
+    find $2 -mindepth 1 -delete
+    ./simple_example kOldestSmallestSeqFirst $1 $2 $3 $4 0 0 $5 $6 $7 $8
+    cp $2/LOG $3/LOG_OSSF
+    rocksdb_size=$(du -sk $2 | awk '{ printf "%dK\n", $1 }')
+    echo "kOldestSmallestSeqFirst: $rocksdb_size" >> $3/rocksdb_size.txt
 }
 
 run_enumerate() {
